c19329a2f39ac154d1bbcbe14e64c53e8f78c11a
KevinGarciaCortez <105743455+KevinGarciaCortez@users.noreply.github.com>
2022-05-24 11:18:58 -0700

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2;
+            Console.WriteLine("Ingrese un número");
+            
+            try
+            {
+                num1 = float.Parse(Console.ReadLine());
+                num2 = num1 * num1;
+                Console.WriteLine("Cuadrado: " + num2);
+            }
+            
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally {  }
+            Console.ReadKey();
+        }
+    }
+}
